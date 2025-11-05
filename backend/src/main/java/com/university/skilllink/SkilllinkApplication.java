4cf043d8a4885d920b8174f55931cca0c697e643
MathivanyECS <mathivmathy@gmail.com>
2025-11-05 17:37:03 +0530

    feat: initialize  backend (Spring Boot)

---
+++ backend/src/main/java/com/university/skilllink/SkilllinkApplication.java
@@ -0,0 +1,13 @@
+package com.university.skilllink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilllinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilllinkApplication.class, args);
+	}
+
+}
