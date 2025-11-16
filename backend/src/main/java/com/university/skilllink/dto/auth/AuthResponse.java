5ff0c8a1e8c61d64a5b7fe5191d637e5a4f361a3
MathivanyECS <mathivmathy@gmail.com>
2025-11-16 10:07:27 +0530

    feat: create AuthResponse  DTOs

--- backend/src/main/java/com/university/skilllink/dto/auth/AuthResponse.java
+++ backend/src/main/java/com/university/skilllink/dto/auth/AuthResponse.java
@@ -1,5 +1,23 @@
 package com.university.skilllink.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
-    
+
+    private String token;
+    private String tokenType;
+    private UserDTO user;
+
+    public AuthResponse(String token, UserDTO user) {
+        this.token = token;
+        this.tokenType = "Bearer";
+        this.user = user;
+    }
 }
