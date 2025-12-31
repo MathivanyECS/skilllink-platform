8e90861a370beec74d22826734da3d9456dc8aae
MathivanyECS <mathivmathy@gmail.com>
2025-12-31 10:35:26 +0530

    feat: create Footer component for application layout

---
+++ frontend/src/components/common/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black py-6 text-center text-gray-400 text-sm">
+      <p>
+        © 2025 <span className="text-primary">SkillLink</span>. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
