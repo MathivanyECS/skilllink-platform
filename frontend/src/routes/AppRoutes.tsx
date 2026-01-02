2cafc42c1e2e23eb9c390e3fc8c549c5e11ca86c
MathivanyECS <mathivmathy@gmail.com>
2026-01-02 08:09:05 +0530

    feat: refine app routing and protected routes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,13 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+
 import GetStarted from "../pages/GetStarted";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import UserDashboard from "../pages/UserDashboard";
+import AdminDashboard from "../pages/AdminDashboard";
+import CreateProfile from "../pages/CreateProfile";
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<GetStarted />} />
     <Route path="/login" element={<Login />} />
+    <Route path="/dashboard" element={<UserDashboard />} />
+    <Route path="/admin-dashboard" element={<AdminDashboard />} />
     <Route path="/register" element={<Register />} />
+    <Route path="/create-profile" element={<CreateProfile />} />
   </Routes>
 );
 
